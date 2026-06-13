9f5432f11b845660777f05ed6fc34dc0220f22e9
Levan Sophromadze <109528742+sopromaa@users.noreply.github.com>
2026-06-13 13:23:45 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "GAMOEXIRE GARET";
+  question.innerHTML = "Doing a happy daaance";
   gif.src =
     "https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp";
 });
